46adfd93e70b2161160d4d9d9464dfe666ed11a5
valsorym <valsorym.e@gmail.com>
2023-10-16 15:02:11 +0300

    Fixed problems with pinned tabs. Fixes #1

--- README.md
+++ README.md
@@ -1,21 +1,74 @@
-# Smart Tabs
+[![Stay with Ukraine](https://img.shields.io/static/v1?label=Stay%20with&message=Ukraine%20♥&color=ffD700&labelColor=0057B8&style=flat)](https://u24.gov.ua/)
 
-The `Smart Tabs` extension is used to automatically move tab with modified content closer to the left or right edge of the list of tabs.
+# Smart Tabs for Visual Studio Code
+
+Enhance your tab management in Visual Studio Code with `Smart Tabs`! This extension automatically repositions tabs with modified content, making them easily accessible, especially when dealing with a multitude of open files.
 
 ## Features
 
-When many tabs are open in the editor, and changes occur only in some, and the work process requires regular switching between them, then such tabs are moved to the beginning (or end) of the list of tabs for faster interaction between them.
+- Intelligent Tab Movement: Once you modify content in a tab, Smart Tabs will automatically move it closer to the left or right edge of your tab list, depending on your preference.
 
-## Extension Settings
+- Respect for Pinned Tabs: If you've pinned certain tabs to ensure they remain in specific positions, Smart Tabs will recognize this and ensure that these pinned tabs aren't disturbed by any automatic movements.
 
-The following parameters can be used to configure the extension:
+- Configurable Fixed Tab Positions: You can specify a set number of tabs that always remain at the beginning or end of your tab list. These tabs will not be impacted by the automatic tab movement behavior of Smart Tabs.
 
-- `smart-tabs.activeFirst` - Boolean value as true or false that determines the direction of movement of the modified tab. If true (default), the tab will be moved to the beginning of the tab list.
-- `smart-tabs.fixedTabs` - An integer value from 1 to 9 that indicates the number of fixed tabs from the beginning or end of the tab list (depending on the activeFirst value). Changing data in pinned tabs does not move the tab to the beginning or end of the tab list.
+## How It Works
+
+1. Upon a content modification in any tab, the extension triggers the smart-tabs.moveTab command.
+
+2. Smart Tabs then checks your specified configurations to determine the desired behavior.
+
+3. Before executing the move, it takes into account any pinned tabs and calculates the right position for the modified tab.
+
+4. The tab is finally repositioned using the VS Code `moveActiveEditor` command.
+
+
+## Configuration Options
+
+### smart-tabs.activeFirst
+
+**Type**: Boolean
+**Default**: false
+
+Determines the direction of movement for the modified tab.
+
+- If set to true, the modified tab will be moved towards the beginning (left side) of the tab list.
+- If set to false, the tab will be moved towards the end (right side).
+
+## smart-tabs.fixedTabs
+
+**Type**: Integer (Range: 1-9)
+**Default**: 5
+
+Specifies the number of tabs that should always remain fixed either at the start or end of the tab list. This depends on the `activeFirst` value. Tabs within this fixed group will not be automatically moved, even if their content is modified.
+
+## Installation
+
+1. Open Visual Studio Code and Navigate to Extensions.
+2. Search for `Smart Tabs` by `Valsorym`.
+3. Click `install` and enjoy enhanced tab management!
+
+
+## Feedback and Contribution
+
+Your feedback is valuable! If you have any suggestions, issues, or would like to contribute, please raise an issue on our [GitHub repository](https://github.com/valsorym/vscode-smart-tabs).
+
+## License
+
+Copyright valsorym, 2022-2023.
 
 ## Release Notes
 
 Improved performance of the extension.
+Fixed errors when working with pinned tabs.
+
+*P.s. Thanks to [huyz](https://github.com/huyz) for finding this problem.*
+
+### 0.1.0
+
+- Change icon.
+- Fixed problems with moving tabs in front of pinned tabs.
+- Fixed problems with moving pinned tabs.
 
 ### 0.0.3
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,121 @@
-// SMART TABS
-// Automatically move tab with modified content closer
-// to the left or right edge of the list of tabs.
-//
-// Copyright valsorym <i@valsorym.com>, 2022.
+/**
+ * SMART TABS EXTENSION FOR VISUAL STUDIO CODE
+ *
+ * Description:
+ * The Smart Tabs extension is designed to improve the tab management
+ * experience in Visual Studio Code. It automatically repositions the tab
+ * containing modified content closer to the left or right edge of the list
+ * of tabs, depending on the user's preference. The aim is to make the active
+ * tab more accessible and intuitive, especially when dealing with multiple
+ * open files.
+ *
+ * Features:
+ * 1. Moves the modified tab closer to the left or right edge based on the
+ *    user's preference.
+ * 2. Respects pinned tabs: The extension identifies pinned tabs and ensures
+ *    that the moved tab does not override or disturb them.
+ * 3. Configurable fixed tab positions: A set number of tabs can be kept fixed
+ *    at the start and won't be affected by automatic movements.
+ *
+ * How It Works:
+ * - When content in a tab is modified, the extension triggers the
+ *   `smart-tabs.moveTab` command.
+ * - The extension then checks the configuration to determine the desired
+ *   behavior, i.e., whether to move the active tab towards the left or right
+ *   and how many tabs should remain fixed at their positions.
+ * - Before moving the tab, the extension considers any pinned tabs and
+ *   calculates the desired position for the modified tab accordingly.
+ * - The tab is then repositioned using the VS Code `moveActiveEditor` command.
+ *
+ * Configuration Options:
+ * - `fixedTabs`: Determines the number of tabs that remain fixed at the
+ *    start or end of the tab list. Default is 5.
+ * - `activeFirst`: A boolean to determine the direction of movement.
+ *    If set to `true`, modified tabs move towards the left; if `false`,
+ *    they move towards the right. Default is `false`.
+ *
+ * Copyright valsorym <i@valsorym.com>, 2022-2023.
+ */
+
 import * as vscode from 'vscode';
 
 
-// The first/last number of tabs that don't respond to changes.
+// FIXED_TABS
+// This constant defines the number of tabs at the beginning (or end,
+// based on the user's configuration) that should remain in their fixed
+// positions and not be affected by the automated tab movement behavior
+// implemented by this extension.
+//
+// For example, if set to 5, the first (or last) five tabs will not be
+// automatically moved when their content is modified. This is useful for
+// keeping frequently accessed or essential files always accessible at known
+// positions.
+//
+// Default value: 5
 const FIXED_TABS = 5;
 
-// The direction of moving tabs to the left (true) or to the right (false).
+// ACTIVE_FIRST
+// A boolean constant that determines the direction of automatic tab movement.
+//
+// - If set to `true`, tabs with modified content will be moved towards the
+//   beginning (left side) of the tab list.
+// - If set to `false`, they will be moved towards the end (right side).
+//
+// This allows users to customize the behavior based on their personal
+// workflow and preferences regarding tab positions.
+//
+// Default value: false (Tabs move towards the right).
 const ACTIVE_FIRST: boolean = false;
 
-// Returns first not pinned tab number or zero.
-function getNotPinnedTabNum(activeGroup: vscode.TabGroup): number {
+
+// getPositionAfterLastPinnedTab(activeGroup: vscode.TabGroup): number
+//
+// This function computes and returns the position immediately after the last
+// pinned tab in the active tab group. If no tabs are pinned, it returns 0.
+//
+// Pinned tabs are tabs that the user has explicitly locked in their current
+// position, preventing them from being automatically moved or closed.
+// By considering pinned tabs, this function ensures that the automated
+// movement behavior respects the user's manual tab arrangements.
+//
+// @param {vscode.TabGroup} activeGroup - the active group of tabs in the
+//     current VS Code window.
+// @returns {number} - the position immediately after the last pinned tab
+//     or 0 if no tabs are pinned.
+function getPositionAfterLastPinnedTab(
+    activeGroup: vscode.TabGroup,
+): number {
+    let lastPinnedIndex = -1;
     for (let i = 0; i < activeGroup.tabs.length; i++) {
-        // Trying to find the first unpinned tab.
         const tab = activeGroup.tabs[i];
-        if (!tab.isPinned) {
-            return i;
+        if (tab.isPinned) {
+            lastPinnedIndex = i;
         }
     }
 
-    return 0;
+    return lastPinnedIndex + 1;
 }
 
-// Returns current active tab number or zero. If fromLeft is true tabs
-// are counted from the left, otherwise from the right. 
+// getActiveTabNum(activeGroup: vscode.TabGroup, fromLeft: boolean): number
+//
+// This function determines the position of the currently active tab (i.e.,
+// the tab in focus) in the tab list. The position can be counted either
+// from the left (beginning) or from the right (end) based on the `fromLeft`
+// parameter.
+//
+// - If `fromLeft` is true, it returns the zero-based index of the active tab
+//   from the left.
+// - If `fromLeft` is false, it returns the zero-based index from the right.
+//
+// This functionality is useful for deciding which direction to move the
+// active tab, based on the user's configuration and preference.
+//
+// @param {vscode.TabGroup} activeGroup - the active group of tabs in the
+//    current VS Code window.
+// @param {boolean} fromLeft - flag to determine direction of counting;
+//    true for counting from the left, false for the right.
+// @returns {number} - zero-based index of the active tab based on the
+//    specified direction.
 function getActiveTabNum(
     activeGroup: vscode.TabGroup,
     fromLeft: boolean,
@@ -47,9 +136,21 @@ function getActiveTabNum(
     return 0;
 }
 
-// Activate extension.
+// Activate the Smart Tabs extension.
+// This function is responsible for subscribing to the `smart-tabs.moveTab`
+// command and attaching the necessary behavior to handle tab movements when
+// a document is modified. The behavior is determined by the extension's
+// configuration settings.
+//
+// During the activation process, the extension:
+// 1. Reads the configuration options set by the user in the VS Code settings.
+// 2. Binds the tab movement behavior to the `smart-tabs.moveTab` command.
+// 3. Listens for changes in any document and triggers the tab movement
+//    command when modifications are detected.
 export function activate(context: vscode.ExtensionContext) {
-    // Subscribe on smart-tabs.moveTab command.
+    // Retrieve configuration settings.
+    // The user can customize the behavior of the extension through VSCode
+    // settings.If the settings aren't provided, default values are used.
     context.subscriptions.push(
         vscode.commands.registerCommand('smart-tabs.moveTab', () => {
             // Get configs.
@@ -59,8 +160,10 @@ export function activate(context: vscode.ExtensionContext) {
             const fixedTabs = config.get('fixedTabs', FIXED_TABS);
             const activeFirst = config.get('activeFirst', ACTIVE_FIRST);
 
-            // Get the active group of tabs.
-            // Stop the command execution if there is no active group of tabs.
+            // Identify the currently active tab group.
+            // The extension operates on the active tab group in the
+            // VSCode window. If no active group is found, the command
+            // execution is halted.
             const activeGroup = vscode.window.tabGroups.all.filter(
                 (group) => group.isActive
             )[0];
@@ -69,24 +172,44 @@ export function activate(context: vscode.ExtensionContext) {
                 return;
             }
 
-            // Get the index of the current tab (active editor).
-            // Abort command execution if the active tab is in the Fixed group.
-            const activeTabNum = getActiveTabNum(activeGroup, activeFirst);
-            if (activeTabNum < fixedTabs) {
+            // Verify that the active tab is not pinned.
+            // Pinned tabs are excluded from the automated movement behavior.
+            const activeTab = activeGroup.activeTab;
+            if (activeTab?.isPinned) {
                 return;
             }
 
-            // Depending on the direction of movement, move the tab
-            // to the extreme position.
+            // Compute the position right after the last pinned tab.
+            // This position is used as a reference point for moving
+            // the modified tab.
+            const offset = getPositionAfterLastPinnedTab(activeGroup);
+
+            // Determine the position of the active tab.
+            const activeTabNum = getActiveTabNum(activeGroup, activeFirst);
+
+            // Move the modified tab based on the user's preferred direction.
+            // If `activeFirst` is set to true, the tab is moved towards the
+            // left; otherwise, it is moved to the extreme right.
             if (activeFirst) {
+                // If the active tab is within the fixed group (as defined
+                // by user settings), it is exempt from the movement logic,
+                // and the command execution is aborted.
+                if (activeTabNum < fixedTabs + offset) {
+                    return;
+                }
+
                 vscode.commands.executeCommand('moveActiveEditor', {
-                    to: 'first',
-                    by: 'tab',
-                    // When moving the tab to the left, we take into
-                    // account the tabs pinned by the user.
-                    value: activeTabNum - getNotPinnedTabNum(activeGroup),
+                    to: 'position',
+                    value: offset + 1,
                 });
             } else {
+                // If the active tab is within the fixed group (as defined
+                // by user settings), it is exempt from the movement logic,
+                // and the command execution is aborted.
+                if (activeTabNum < fixedTabs) {
+                    return;
+                }
+
                 vscode.commands.executeCommand('moveActiveEditor', {
                     to: 'last',
                     by: 'tab',
@@ -103,5 +226,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 // Deactivate extension.
 export function deactivate() {
-    vscode.window.showErrorMessage('Successfully deactivated extension.');
+    vscode.window.showErrorMessage('Successfully deactivated ' +
+        'Smart-Tabs extension.');
 }
